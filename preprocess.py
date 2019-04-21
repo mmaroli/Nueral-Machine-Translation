ac12dbebc56e4b10e5702293d3a1d9e0500ea8c2
mmaroli <mmaroli@ucla.edu>
2019-04-21 16:45:45 -0700

    Change function call to class function call

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import unicodedata
 import re
 
 class Preprocessor:
+    """ Preprocessor Class """ 
     def __init__(self, path_to_file=None):
         self.path_to_file = path_to_file
 
@@ -11,7 +12,7 @@ class Preprocessor:
         return ''.join(c for c in unicodedata.normalize('NFC', string) if unicodedata.category(c) != 'Mn')
 
     def preprocess(self, sentence):
-        sentence = unicode_to_ascii(sentence.lower().strip())
+        sentence = self.unicode_to_ascii(sentence.lower().strip())
         sentence = re.sub(r"([?.!,¿])", r" \1", sentence) # separate punctuation into its own token
         sentence = re.sub(r'[" "]+', " ", sentence)
         sentence = re.sub(r"[^a-zA-z?.!,¿]+", " ", sentence)

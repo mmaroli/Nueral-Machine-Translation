f22c1ba7fee8455f5cb4326baa0e609bccb4540e
Manav Maroli <manav.maroli@leafgroup.com>
2019-05-05 13:47:45 -0700

    Test for merge

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from load_data import DataLoader
+import random
 
 
 
@@ -7,4 +8,3 @@ class Seq2Seq:
     def __init__(self):
         self.dl = DataLoader(path_to_file='language-files/por.txt')
         self.input_tensor, self.targ_tensor, self.inp_lang_tokenizer, self.targ_lang_tokenizer = self.dl.load_dataset()
-        

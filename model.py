9b55398eff9e75f7af5e2c1dd80aae63e77c8984
Manav Maroli <manav.maroli@leafgroup.com>
2019-05-13 18:30:18 -0700

    Implemented Encoder layer

--- model.py
+++ model.py
@@ -20,6 +20,44 @@ class Seq2Seq:
         self.dataset = tf.data.Dataset.from_tensor_slices((self.input_tensor_train, self.targ_tensor_train)).shuffle(BUFFER_SIZE)
         self.dataset = self.dataset.batch(self.batch_size, drop_remainder=True)
 
+class Encoder(tf.keras.Model):
+    """
+        encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
+
+        # sample input
+        sample_hidden = encoder.initialize_hidden_state()
+        sample_output, sample_hidden = encoder(example_input_batch, sample_hidden)
+        print ('Encoder output shape: (batch size, sequence length, units) {}'.format(sample_output.shape))
+        print ('Encoder Hidden state shape: (batch size, units) {}'.format(sample_hidden.shape))
+    """
+    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+        super()
+        self.batch_sz = batch_sz
+        self.enc_units = enc_units
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.enc_units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
+
+    def call(self, x, hidden):
+        x = self.embedding(x)
+        output, state = self.gru(x, initial_state=hidden)
+        return output, state
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_sz, self.enc_units))
+
+
+class BahdanauAttention(tf.keras.Model):
+    def __init__(self, units):
+        super()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+
+
 
 if __name__ == '__main__':
     model = Seq2Seq(batch_size=64)
